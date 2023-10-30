ec6f9419c1b6422b11a109ba8bc58b5a51fb2857
NITESH-SINGH-SE <96736302+NITESH-SINGH-SE@users.noreply.github.com>
2023-10-30 21:28:09 +0530

    added categories, made the responsiveness better

---
+++ script.js
@@ -0,0 +1,80 @@
+const API_KEY = "405cc418b6904b15bb3b0247b9bd08cc";
+const url = "https://newsapi.org/v2/everything?q=";
+
+window.addEventListener("load", () => fetchNews("India"));
+
+function reload() {
+    window.location.reload();
+}
+
+async function fetchNews(query){
+    const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+    const data = await res.json();
+    console.log(data);
+    bindData(data.articles);
+}
+
+function bindData(articles){
+    const cardsContainer = document.getElementById('cards-container');
+    const newsCardTemplate = document.getElementById('template-news-card');
+
+    cardsContainer.innerHTML = '';
+
+    articles.forEach((article) => {
+        if(!article.urlToImage) return;
+        const cardClone = newsCardTemplate.content.cloneNode(true);
+        fillDataInCard(cardClone, article);
+        cardsContainer.appendChild(cardClone);
+    });
+}
+
+function fillDataInCard(cardClone, article){
+    const newsImg = cardClone.querySelector('#news-img');
+    const newsTitle = cardClone.querySelector('#news-title');
+    const newsSource = cardClone.querySelector('#news-source');
+    const newsDesc = cardClone.querySelector('#news-desc');
+
+    newsImg.src = article.urlToImage;
+    newsTitle.innerHTML = article.title;
+    newsDesc.innerHTML = article.description;
+
+    const date = new Date(article.publishedAt).toLocaleString("en-US", {
+        timeZone: "Asia/Jakarta"
+    });
+
+    newsSource.innerHTML = `${article.source.name} . ${date}`;
+
+    cardClone.firstElementChild.addEventListener('click', ()=>{
+        window.open(article.url, "_blank");
+    })
+}
+
+let curSelectedNav = null;
+
+function onNavItemClick(id){
+    fetchNews(id);
+    const navItem = document.getElementById(id);
+    curSelectedNav?.classList.remove('active');
+    curSelectedNav = navItem;
+    curSelectedNav.classList.add('active');
+}
+
+const searchButton = document.getElementById('search-button');
+const searchText = document.getElementById('search-text');
+
+searchButton.addEventListener('click', ()=>{
+    const query = searchText.value;
+    if(!query)  return;
+    fetchNews(query);
+    curSelectedNav?.classList.remove('active');
+    curSelectedNav = null;
+})
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
